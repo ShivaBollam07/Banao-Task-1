02e64217f771332c6ebb8ae7ae712e208ab3f0ae
Shiva <98306948+ShivaBollam07@users.noreply.github.com>
2023-05-19 00:01:19 +0530

    Add files via upload

---
+++ src/utils/contant.js
@@ -0,0 +1,29 @@
+import P1 from "../assets/firstLogo.png";
+import P2 from "../assets/secondLogo.png";
+import P3 from "../assets/thirdLogo.png";
+import P4 from "../assets/fourthLogo.png";
+
+export const recGroup = [
+  {
+    name: "Leisure",
+    avatar: P4,
+    text: "followed",
+    color: "#fff",
+    bgcolor: "#000",
+  },
+  {
+    name: "Activism",
+    avatar: P1,
+    text: "follow",
+  },
+  {
+    name: "MBA",
+    avatar: P2,
+    text: "follow",
+  },
+  {
+    name: "Philosophy",
+    avatar: P3,
+    text: "follow",
+  },
+];
